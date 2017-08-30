87f19fb637953110ba298fbe65889b5fff6b92be
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2017-08-30 11:38:22 +0200

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("kevin is gay");
+        System.out.println("kevin is homo");
     }
 }
